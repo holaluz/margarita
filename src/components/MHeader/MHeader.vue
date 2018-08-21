c013a5cb7014b69160ef06135290547facfa1c2d
germanrcuriel <germix@germix.net>
2018-08-21 10:46:26 +0200

    Add mobile navigation PoC

--- src/components/MHeader/MHeader.vue
+++ src/components/MHeader/MHeader.vue
@@ -1,97 +1,168 @@
 <template>
-  <div class="bla">
+  <v-layout
+    tag="header"
+    row
+    wrap
+  >
     <v-layout
-      tag="header"
-      row
-      wrap
+      hidden-sm-and-down
+      style="height: 116px;"
     >
-      <v-layout
-        xs12
-        style="height: 100px;"
-      >
-        <v-flex
-          xs12
-          sm6
-          md4
-          align-content-center
-        >
+      <v-layout md12>
+        <v-flex align-content-center>
           <img
             :src="logoImage"
             d-block
           >
         </v-flex>
-        <v-spacer />
-        <v-flex
-          xs12
-          sm6
-          md4
-          text-xs-right
-        >
-          <v-btn color="success">Contratar</v-btn>
-          <v-btn color="warning">Zona Cliente</v-btn>
+
+        <v-flex text-md-right>
+          <v-btn
+            flat
+            color="success"
+          >
+            Contratar
+          </v-btn>
+          <v-btn
+            flat
+            color="warning"
+          >
+            Zona Cliente
+          </v-btn>
         </v-flex>
       </v-layout>
-      <v-flex
-        xs12
-        style="height: 100px;"
-      >
-        <v-menu
-          open-on-hover
-          bottom
-          offset-y
-        >
-          <v-btn
-            slot="activator"
-            flat
-            color="primary"
-          >
-            Link one
-          </v-btn>
-          <v-list>
-            <v-list-tile>
-              <v-list-tile-title>Prueba 1</v-list-tile-title>
-            </v-list-tile>
-            <v-list-tile>
-              <v-list-tile-title>Prueba 2</v-list-tile-title>
-            </v-list-tile>
-            <v-list-tile>
-              <v-list-tile-title>Prueba 3</v-list-tile-title>
-            </v-list-tile>
-            <v-list-tile>
-              <v-list-tile-title>Prueba 4</v-list-tile-title>
-            </v-list-tile>
-          </v-list>
-        </v-menu>
-        <v-menu
-          open-on-hover
-          bottom
-          offset-y
-        >
-          <v-btn
-            slot="activator"
-            flat
-            color="secondary"
-          >
-            Link two
-          </v-btn>
-          <v-list>
-            <v-list-tile>
-              <v-list-tile-title>Prueba 1</v-list-tile-title>
-            </v-list-tile>
-            <v-list-tile>
-              <v-list-tile-title>Prueba 2</v-list-tile-title>
-            </v-list-tile>
-            <v-list-tile>
-              <v-list-tile-title>Prueba 3</v-list-tile-title>
-            </v-list-tile>
-            <v-list-tile>
-              <v-list-tile-title>Prueba 4</v-list-tile-title>
-            </v-list-tile>
-          </v-list>
-        </v-menu>
-      </v-flex>
     </v-layout>
-  </div>
+
+    <v-layout
+      row
+      hidden-md-and-up>
+      <v-flex xs12>
+        <v-card height="48px">
+          <v-layout>
+            <v-flex style="flex-grow: 0">
+              <v-btn
+                flat
+                icon
+                @click.stop="drawer = !drawer"
+              >
+                <v-icon>mdi-dots-vertical</v-icon>
+              </v-btn>
+            </v-flex>
+            <v-flex style="padding: 12px 0; display: flex;">
+              <v-card-media
+                :src="logoImage"
+                height="24px"
+                style="flex: 0 0 90px;"
+              />
+            </v-flex>
+            <v-flex
+              xs7
+              sm7
+              justify-end
+              align-center
+              style="display: flex;"
+            >
+              <v-btn
+                depressed
+                small
+                color="primary"
+              >
+                Contratar
+              </v-btn>
+            </v-flex>
+          </v-layout>
+        </v-card>
+      </v-flex>
+
+      <v-navigation-drawer
+        v-model="drawer"
+        absolute
+        temporary
+      >
+        <v-tabs
+          v-model="language"
+          dark
+          style="background-color: #505050"
+        >
+          <v-tab
+            v-for="item in languages"
+            :key="item.label"
+            @click="null"
+          >
+            {{ item.label }}
+          </v-tab>
+        </v-tabs>
+        <v-list>
+          <template v-for="item in menu">
+            <v-list-group
+              v-if="item.items"
+              v-model="item.active"
+              :key="item.title"
+            >
+              <v-list-tile
+                v-if="item.items"
+                slot="activator"
+              >
+                <v-list-tile-content>
+                  <v-list-tile-title>
+                    {{ item.title }}
+                  </v-list-tile-title>
+                </v-list-tile-content>
+              </v-list-tile>
+
+              <v-list-tile
+                v-for="subitem in item.items"
+                :key="subitem.title"
+                @click="null"
+              >
+                <v-list-tile-content>
+                  <v-list-tile-title style="padding-left: 12px;">
+                    {{ subitem.title }}
+                  </v-list-tile-title>
+                </v-list-tile-content>
+              </v-list-tile>
+            </v-list-group>
+
+            <v-list-tile
+              v-else
+              :key="item.title"
+              @click="null"
+            >
+              <v-list-tile-content>
+                <v-list-tile-title>
+                  {{ item.title }}
+                </v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </template>
+        </v-list>
+        <v-flex style="padding: 0 12px;">
+          <v-btn
+            block
+            color="secondary"
+            style="margin: 12px 0"
+          >
+            Zona Cliente
+          </v-btn>
+          <v-btn
+            block
+            color="primary"
+            style="margin: 12px 0"
+          >
+            Contratar
+          </v-btn>
+          <v-btn
+            block
+            flat
+            color="green"
+            style="margin: 12px 0"
+          >
+            100% Luz Verde
+          </v-btn>
+        </v-flex>
+      </v-navigation-drawer>
+    </v-layout>
+  </v-layout>
 </template>
 
 <script>
@@ -99,12 +170,64 @@ export default {
   name: 'MHeader',
   data () {
     return {
-      logoImage: 'https://alta.holaluz.com/images/logo.svg'
+      drawer: null,
+      language: 'es',
+      logoImage: 'https://alta.holaluz.com/images/logo.svg',
+      languages: [
+        { label: 'ES' },
+        { label: 'CA' },
+        { label: 'EN' }
+      ],
+      menu: [
+        {
+          title: 'Conócenos',
+          items: [
+            { title: 'Conócenos' },
+            { title: 'La luz justa' },
+            { title: 'Energía Verde' },
+            { title: 'Luz amiga' }
+          ]
+        }, {
+          title: 'Tarifas Hogares',
+          items: [
+            { title: 'Tarifas Hogares' },
+            { title: 'Encuentra tu tarifa de luz' },
+            { title: 'Tarifa de Luz Un Precio' },
+            { title: 'Tarifa de Luz Dos Precios' },
+            { title: 'Tarifa Coche Eléctrico' },
+            { title: 'Tarifa Gas' }
+          ]
+        }, {
+          title: 'Tarifas Negocios',
+          items: [
+            { title: 'Tarifas Negocios' },
+            { title: 'Encuentra tu tarifa de luz' },
+            { title: 'Tarifa de Luz Un Precio' },
+            { title: 'Tarifa de Luz Dos Precios' },
+            { title: 'Tarifa de Luz Tres Precios' },
+            { title: 'Casos Especiales de luz' }
+          ]
+        }, {
+          title: 'Autoconsumo',
+          items: [
+            { title: 'Autoconsumo' },
+            { title: 'Fórmula Solar Flexible' },
+            { title: 'Fórmula Solar' },
+            { title: 'Baterías Inteligentes' }
+          ]
+        }, {
+          title: 'Productores'
+        }, {
+          title: 'Blog'
+        }, {
+          title: 'Contacto'
+        }
+      ]
     }
   }
 }
 </script>
 
-<style>
+<style scoped>
 
 </style>
