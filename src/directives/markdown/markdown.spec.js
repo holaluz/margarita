192cf65945cb962fdf1908ea9810c3c4d358cd52
Blai Samitier <blai.samitier@gmail.com>
2019-10-04 17:42:22 +0200

    add title to test

--- src/directives/markdown/markdown.spec.js
+++ src/directives/markdown/markdown.spec.js
@@ -1,18 +1,25 @@
 import { render } from '@testing-library/vue'
 import './markdown'
 
+const markdown = `
+# title
+**bold**
+__italic__
+[anchor](url.com){.class}
+`
+
 const component = {
-  template: `
-    <div
-      v-markdown="'**bold** __italic__ [anchor](url.com){.class}'"
-    />
-  `,
+  template: `<div v-markdown="markdown" />`,
+  data() {
+    return { markdown }
+  },
 }
 
 describe('Markdown directive', () => {
-  it('renders bold, italic, anchors and classes', () => {
+  it('renders title, bold, italic, anchors and classes', () => {
     const { getByText } = render(component)
 
+    getByText('title')
     getByText('bold')
     getByText('italic')
     const { classList } = getByText('anchor')
