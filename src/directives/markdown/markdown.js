625bd90b52be71a6a5f1d6d02a9926d7c9a90790
Blai Samitier <blai.samitier@gmail.com>
2019-10-07 15:44:22 +0200

    Change delimiter for html tags

--- src/directives/markdown/markdown.js
+++ src/directives/markdown/markdown.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import markdownIt from 'markdown-it'
 import attrs from 'markdown-it-attrs'
 
-const md = new markdownIt().use(attrs)
+const md = new markdownIt().use(attrs, {
+  leftDelimiter: '<<',
+  rightDelimiter: '>>',
+})
 
 Vue.directive('markdown', (el, { value }) => {
   el.innerHTML = md.render(value)
