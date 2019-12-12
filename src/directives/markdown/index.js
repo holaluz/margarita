e13054cb58b18241f5fbf4d36c69f52ddd9686f7
Blai Samitier <blai.samitier@gmail.com>
2019-12-12 13:22:41 +0100

    small refactor for cleanup

--- src/directives/markdown/index.js
+++ src/directives/markdown/index.js
@@ -1,41 +1,41 @@
 import snarkdown from 'snarkdown'
 
-const REGEX_PATTERN = /<<(.*)>>/g
-const CLASS_PATTERN = /^\.[\w-]+$/
-const ID_PATTERN = /^#[\w-]+$/
+const ATTR_REGEX = /<<(.*)>>/g
+const CLASS_REGEX = /^\.[\w-]+$/
+const ID_REGEX = /^#[\w-]+$/
 
 export default (el, { value }) => {
   el.innerHTML = snarkdown(value)
-
-  setClassNames(el)
+  setMarkdownAttributes(el)
 }
 
-function setClassNames(el) {
-  const nodes = el.childNodes
-
-  for (let i = 0; i < nodes.length; i++) {
-    const node = nodes[i]
-    if (node.tagName) {
-      setClassNames(node)
-    } else if (i > 0) {
-      const attrs = node.textContent
-      const match = REGEX_PATTERN.exec(attrs)
-
-      if (match) {
-        const attrList = match[1].split(' ')
-        const prevNode = nodes[i - 1]
-
-        attrList.forEach((attr = '') => {
-          attr = attr.trim()
-          if (attr.match(CLASS_PATTERN)) {
-            prevNode.classList.add(attr.replace('.', ''))
-          } else if (attr.match(ID_PATTERN)) {
-            prevNode.id = attr.replace('#', '')
-          }
-        })
-
-        node.textContent = node.textContent.replace(REGEX_PATTERN, '')
+// Adds attributes (class or id) to an HTML node if the next text node has markdown attributes.
+function setMarkdownAttributes({ childNodes }) {
+  for (let i = 0; i < childNodes.length; i++) {
+    const node = childNodes[i]
+    // If the next HTML node is an HTML tag, we parse its contents.
+    if (node.tagName) setMarkdownAttributes(node)
+    // If the next HTML node is a text node we check if there are markdown attributes in it.
+    else if (i > 0) {
+      const attrMatch = ATTR_REGEX.exec(node.textContent)
+      if (attrMatch) {
+        // If there are markdown attributes, we add them to the previous HTML node.
+        attrMatch[1]
+          .split(' ')
+          .forEach(attr => addNodeAttr(childNodes[i - 1], attr))
+        // We remove the markdown attributes from the DOM
+        node.textContent = node.textContent.replace(ATTR_REGEX, '')
       }
     }
+  }
+}
+
+// Adds class or id to an HTML node
+function addNodeAttr(node, attribute = '') {
+  const attr = attribute.trim()
+  if (attr.match(CLASS_REGEX)) {
+    node.classList.add(attr.replace('.', ''))
+  } else if (attr.match(ID_REGEX)) {
+    node.id = attr.replace('#', '')
   }
 }
