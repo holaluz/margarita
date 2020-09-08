6dfe11dae320ced27133d881f2c09c810d0962a6
Adrià Fontcuberta <afontcu@gmail.com>
2020-09-08 11:04:57 +0200

    Add basic stories

---
+++ src/components/MaHidden/MaHidden.stories.js
@@ -0,0 +1,36 @@
+import { storiesOf } from '@storybook/vue'
+
+import MaHidden from '@margarita/components/MaHidden'
+
+storiesOf('Hidden', module)
+  .add('Hidden', () => {
+    return {
+      components: { MaHidden },
+      template: `
+        <div>
+          <ma-hidden below="tablet">1. I'm hidden below tablet!</ma-hidden>
+          <ma-hidden below="desktop">2. I'm hidden below desktop!</ma-hidden>
+
+          <ma-hidden above="mobile">3. I'm hidden above mobile!</ma-hidden>
+          <ma-hidden above="tablet">4. I'm hidden above tablet!</ma-hidden>
+        </div>
+      `,
+    }
+  })
+  .add('Hidden inline', () => {
+    return {
+      components: { MaHidden },
+      template: `
+        <p>
+          This is some placeholder content
+          <ma-hidden inline below="tablet">
+            <b>1. I'm hidden below tablet!</b>
+          </ma-hidden>
+          <ma-hidden inline above="mobile">
+            <b>3. I'm hidden above mobile!</b>
+          </ma-hidden>
+          to show how inline hidden elements wrap
+        </p>
+      `,
+    }
+  })
