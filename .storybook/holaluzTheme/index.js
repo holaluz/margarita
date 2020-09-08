56a78bfdb574dd08184a66be721ebe3d55dd23d8
Adrià Fontcuberta <afontcu@gmail.com>
2020-09-08 14:56:38 +0200

    Further simplify folders

--- .storybook/holaluzTheme/index.js
+++
@@ -1,9 +0,0 @@
-import { create } from '@storybook/theming/create'
-import brandImage from './holaluz.svg'
-
-export default create({
-  base: 'light',
-  brandTitle: 'Holaluz Margarita',
-  brandUrl: 'https://www.holaluz.com/',
-  brandImage,
-})
--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,5 +1,13 @@
 import { addons } from '@storybook/addons'
-import theme from './holaluzTheme'
+import { create } from '@storybook/theming/create'
+import brandImage from './holaluz.svg'
+
+const theme = create({
+  base: 'light',
+  brandTitle: 'Holaluz Margarita',
+  brandUrl: 'https://www.holaluz.com/',
+  brandImage,
+})
 
 addons.setConfig({
   panelPosition: 'bottom',
