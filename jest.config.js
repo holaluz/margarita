e2aab620631eb8c5d712bef7ef3465fc3cf383cc
Adrià Fontcuberta <afontcu@gmail.com>
2020-12-13 00:38:29 +0100

    Use jest config from npm-scripts

--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,12 @@
-module.exports = {
-  moduleFileExtensions: ['vue', 'js', 'json'],
+const merge = require('lodash.merge')
+const jestConfig = require('@holaluz/npm-scripts').jest
+
+module.exports = merge(jestConfig, {
   transform: {
-    '^.+\\.vue$': 'vue-jest',
-    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$':
-      'jest-transform-stub',
-    '^.+\\.js$': 'babel-jest',
+    '.+\\.(css|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
   },
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
     '^@margarita/(.*)$': '<rootDir>/src/$1',
-    '^@@/(.*)$': '<rootDir>/$1',
   },
   testEnvironment: 'jest-environment-jsdom-sixteen',
-  testMatch: ['**/*.spec.(js|ts)'],
-  setupFilesAfterEnv: ['@holaluz/npm-scripts/jest.setup.js'],
-  watchPlugins: [
-    'jest-watch-typeahead/filename',
-    'jest-watch-typeahead/testname',
-  ],
-  watchPathIgnorePatterns: ['<rootDir>/node_modules/'],
-}
+})
