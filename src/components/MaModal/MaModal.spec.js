15a4aa303149cee1cc5fe0c9c9e5de48e1b2da9e
Adrià Fontcuberta <afontcu@gmail.com>
2020-12-12 13:43:05 +0100

    Add MaModal

---
+++ src/components/MaModal/MaModal.spec.js
@@ -0,0 +1,167 @@
+import {
+  render,
+  fireEvent,
+  waitFor,
+  waitForElementToBeRemoved,
+} from '@testing-library/vue'
+import responsivePlugin from '@margarita/plugins/responsivePlugin'
+import MaModal from './MaModal'
+
+describe('MaModal', () => {
+  test('shows modal content on clicking trigger element', async () => {
+    const { openModal, assertModalIsOpen, queryByRole } = renderComponent()
+
+    expect(queryByRole('dialog')).not.toBeInTheDocument()
+
+    await openModal()
+
+    assertModalIsOpen()
+  })
+
+  test('focus on first focusable element on opening', async () => {
+    const { openModal, getByTestId } = renderComponent({
+      scopedSlots: {
+        content: `
+          <div>
+          <input data-testid="input" />
+          <button>nope</button>
+          </div>
+        `,
+      },
+    })
+
+    await openModal()
+
+    expect(getByTestId('input')).toHaveFocus()
+  })
+
+  // eslint-disable-next-line jest/expect-expect
+  test('removes modal on hitting escape', async () => {
+    const { openModal, container, assertModalIsClosed } = renderComponent()
+
+    await openModal()
+
+    await fireEvent.keyDown(container, { key: 'Escape' })
+
+    await assertModalIsClosed()
+  })
+
+  test('does not send closing event if modal is already closed', async () => {
+    const { emitted, container } = renderComponent()
+
+    await fireEvent.keyDown(container, { key: 'Escape' })
+
+    expect(emitted()).not.toHaveProperty('close')
+  })
+
+  // eslint-disable-next-line jest/expect-expect
+  test('removes modal on clicking close button', async () => {
+    const { openModal, getByTestId, assertModalIsClosed } = renderComponent()
+
+    await openModal()
+
+    await fireEvent.click(getByTestId('close-button'))
+
+    await assertModalIsClosed()
+  })
+
+  // eslint-disable-next-line jest/expect-expect
+  test('removes modal on clicking overlay', async () => {
+    const { openModal, getByTestId, assertModalIsClosed } = renderComponent()
+
+    await openModal()
+
+    await fireEvent.click(getByTestId('overlay'))
+
+    await assertModalIsClosed()
+  })
+
+  // eslint-disable-next-line jest/expect-expect
+  test('removes modal on clicking custom close element from slot', async () => {
+    const { openModal, getByText, assertModalIsClosed } = renderComponent({
+      scopedSlots: {
+        content: `<button @click="props.closeModal">close</button>`,
+      },
+    })
+
+    await openModal()
+
+    await fireEvent.click(getByText('close'))
+
+    await assertModalIsClosed()
+  })
+
+  test('accepts a width prop', async () => {
+    const { openModal, getByRole, updateProps } = renderComponent()
+
+    await openModal()
+
+    expect(getByRole('dialog')).toHaveClass('modal--width-medium')
+
+    await updateProps({ width: 'large' })
+
+    expect(getByRole('dialog')).toHaveClass('modal--width-large')
+  })
+
+  test('removes event listener on destruction', () => {
+    jest.spyOn(document, 'removeEventListener')
+
+    const { unmount } = renderComponent()
+
+    unmount()
+
+    expect(document.removeEventListener).toHaveBeenCalledTimes(1)
+    expect(document.removeEventListener).toHaveBeenCalledWith(
+      'keydown',
+      expect.any(Function)
+    )
+  })
+})
+
+function renderComponent({ props = {}, scopedSlots = null } = {}) {
+  const title = 'my title'
+  const utils = render(
+    MaModal,
+    {
+      props: { title, ...props },
+      scopedSlots: {
+        trigger: `<button @click="props.openModal">open</button>`,
+        content: `<div>modal content</div>`,
+        ...scopedSlots,
+      },
+    },
+    (vue) => {
+      vue.use(responsivePlugin)
+    }
+  )
+
+  const openModal = async () => {
+    await fireEvent.click(utils.getByRole('button', { name: 'open' }))
+    await waitFor(() => expect(utils.getByRole('dialog')).toBeInTheDocument())
+  }
+
+  const assertModalIsClosed = async () => {
+    await waitForElementToBeRemoved(utils.getByRole('dialog'))
+
+    expect(utils.emitted()).toHaveProperty('close')
+    expect(utils.emitted().close).toHaveLength(1)
+  }
+
+  const assertModalIsOpen = () => {
+    expect(utils.emitted()).toHaveProperty('open')
+    expect(utils.emitted().open).toHaveLength(1)
+
+    const modal = utils.getByRole('dialog')
+    expect(modal).toHaveTextContent('modal content')
+    expect(modal).toHaveTextContent(title)
+    expect(modal).toHaveAttribute('aria-modal', 'true')
+    expect(modal).toHaveAttribute('aria-label', title)
+  }
+
+  return {
+    openModal,
+    assertModalIsClosed,
+    assertModalIsOpen,
+    ...utils,
+  }
+}
---
+++ src/components/MaModal/MaModal.vue
@@ -0,0 +1,193 @@
+<template>
+  <div class="modal-wrapper">
+    <slot name="trigger" :openModal="openModal" />
+    <portal-to-body>
+      <transition
+        v-if="showModal"
+        name="modal-transition"
+        appear
+        @after-leave="closeModal"
+      >
+        <div>
+          <div
+            class="modal-overlay"
+            data-testid="overlay"
+            @click="closeModal"
+          />
+          <ma-stack
+            :aria-label="title"
+            :class="`modal--width-${width}`"
+            space="xsmall"
+            role="dialog"
+            aria-modal="true"
+            class="modal"
+          >
+            <div class="modal-header">
+              <span class="modal-title">{{ title }}</span>
+              <button data-testid="close-button" @click="closeModal">x</button>
+            </div>
+            <div ref="modal-content" class="modal-content">
+              <slot :closeModal="closeModal" name="content" />
+            </div>
+          </ma-stack>
+        </div>
+      </transition>
+    </portal-to-body>
+  </div>
+</template>
+
+<script>
+import MaStack from '@margarita/components/MaStack'
+import { Portal } from '@linusborg/vue-simple-portal'
+
+const MODAL_WIDTHS = ['small', 'medium', 'large', 'content']
+
+export default {
+  name: 'MaModal',
+
+  components: {
+    PortalToBody: Portal,
+    MaStack,
+  },
+
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+
+    width: {
+      type: String,
+      default: 'medium',
+      validator: (w) => MODAL_WIDTHS.includes(w),
+    },
+  },
+
+  data() {
+    return {
+      showModal: false,
+    }
+  },
+
+  mounted() {
+    document.addEventListener('keydown', this.escapeHandler)
+
+    this.$once('hook:destroyed', () => {
+      document.removeEventListener('keydown', this.escapeHandler)
+    })
+  },
+
+  methods: {
+    async openModal() {
+      this.showModal = true
+      this.$emit('open')
+
+      await this.focusFirstFocusableElement()
+    },
+
+    closeModal() {
+      // Calling this method will trigger the transition,
+      // which when finished, will disable the Portal
+      // through the `afterLeave` hook callback.
+      // Otherwise no leaving transition happens.
+      this.showModal = false
+      this.$emit('close')
+    },
+
+    escapeHandler(e) {
+      if (this.showModal && e.key === 'Escape') {
+        this.closeModal()
+      }
+    },
+
+    async focusFirstFocusableElement() {
+      const focusableElements =
+        'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])'
+
+      await flushQueue()
+
+      this.$refs['modal-content'].querySelector(focusableElements).focus()
+
+      function flushQueue() {
+        return new Promise((resolve) => setTimeout(resolve, 0))
+      }
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.modal-overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+
+  background-color: rgba(0, 0, 0, 0.25);
+}
+
+.modal {
+  position: fixed;
+
+  // centering
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+
+  // so that it doesn't reach end of viewport
+  width: 96vw;
+
+  // temporary, stylistic stuff
+  background-color: white;
+  box-shadow: 0 25px 50px -12px rgba(0, 0, 0, 0.25);
+  border-radius: 0.5rem;
+}
+
+.modal-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: get-space('xxsmall') get-space('xxsmall') 0;
+}
+
+.modal-title {
+  font-weight: bold;
+  font-size: 1.3rem;
+}
+
+.modal-content {
+  padding: 0 get-space('xxsmall');
+
+  // because fuck standards, that's why
+  // https://github.com/w3c/csswg-drafts/issues/129
+  margin-bottom: get-space('xxsmall');
+
+  // height management
+  max-height: 85vh;
+  overflow-y: auto;
+}
+
+.modal-transition-enter-active,
+.modal-transition-leave-active {
+  transition: all 0.3s cubic-bezier(0.4, 0, 1, 1);
+}
+
+.modal-transition-enter,
+.modal-transition-leave-to {
+  opacity: 0;
+}
+
+.modal--width-small {
+  max-width: 400px;
+}
+.modal--width-medium {
+  max-width: 600px;
+}
+.modal--width-large {
+  max-width: 900px;
+}
+.modal--width-content {
+  max-width: auto;
+}
+</style>
