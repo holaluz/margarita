1ae51f92bd1354abcde9b1ac887f876e7d6b1992
Adrià Fontcuberta <afontcu@gmail.com>
2020-12-14 17:05:31 +0100

    Add comment

--- types/vue.d.ts
+++ types/vue.d.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import { Layout } from './margarita'
 
+// https://vuejs.org/v2/guide/typescript.html#Augmenting-Types-for-Use-with-Plugins
 declare module 'vue/types/vue' {
   interface Vue {
     $layout: Layout
