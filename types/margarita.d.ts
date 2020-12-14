126614510010d197988ce6232f4f0916db9a4bc8
Adrià Fontcuberta <afontcu@gmail.com>
2020-12-14 17:14:38 +0100

    Fix types

--- types/margarita.d.ts
+++ types/margarita.d.ts
@@ -1,6 +1,6 @@
 export declare type Breakpoint = 'mobile' | 'tablet' | 'desktop'
 
 export type Layout = {
-  readonly currentBreakpoint: Breakpoint | null
-  getResponsivePropValue: (_: Breakpoint | Breakpoint[]) => Breakpoint
+  readonly currentBreakpoint: Breakpoint
+  getResponsivePropValue: (_: string | string[]) => string
 }
