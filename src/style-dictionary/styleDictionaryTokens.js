456ee3a8130ec551c4deca8ecc895a493edd9e20
Adrià Fontcuberta <afontcu@gmail.com>
2021-01-11 19:40:52 +0100

    Add missing tokens

--- src/style-dictionary/styleDictionaryTokens.js
+++ src/style-dictionary/styleDictionaryTokens.js
@@ -1,8 +1,12 @@
-const colors = require('../tokens/colors')
-const spacing = require('../tokens/spacing')
 const addTokenType = require('./addTokenType')
+const color = require('../tokens/color')
+const spacing = require('../tokens/spacing')
+const shadow = require('../tokens/shadow')
+const { breakpoints } = require('../tokens/breakpoint')
 
-const spacingTokens = addTokenType(spacing, 'spacing')
-const colorTokens = addTokenType(colors, 'color')
-
-module.exports = { ...colorTokens, ...spacingTokens }
+module.exports = {
+  color: addTokenType(color, 'color'),
+  spacing: addTokenType(spacing, 'spacing'),
+  shadow: addTokenType(shadow, 'shadow'),
+  breakpoint: addTokenType(breakpoints, 'breakpoint'),
+}
