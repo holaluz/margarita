809e2a36a4184f4353ae436af59a168e48f036a2
Adrià Fontcuberta <afontcu@gmail.com>
2021-01-11 19:40:04 +0100

    Generate CSS custom properties file

--- styleDictionary.config.js
+++ styleDictionary.config.js
@@ -1,21 +1,23 @@
 module.exports = {
   source: ['./src/style-dictionary/styleDictionaryTokens.js'],
   platforms: {
+    css: {
+      transformGroup: 'css',
+      files: [
+        {
+          format: 'css/variables',
+          destination: './src/css/tokens.css',
+        },
+      ],
+    },
     scss: {
       transformGroup: 'scss',
       buildPath: './src/scss/variables/',
       files: [
         {
-          destination: '_colors.scss',
-          format: 'scss/map-flat',
-          mapName: 'colors',
-          filter: { type: 'color' },
-        },
-        {
-          destination: '_spacing.scss',
-          format: 'scss/map-flat',
-          mapName: 'layout-spacing',
-          filter: { type: 'spacing' },
+          destination: '_tokens.scss',
+          format: 'scss/map-deep',
+          mapName: 'tokens',
         },
       ],
     },
