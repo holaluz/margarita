39d6865d00bd24e2ecab9bbaa1c39c7e2dfd6589
Eduard Farre Puig <edu.farrep@gmail.com>
2021-01-28 17:33:20 +0100

    Adapt MaText tests

--- src/components/MaText/MaText.spec.js
+++ src/components/MaText/MaText.spec.js
@@ -1,11 +1,7 @@
-/* eslint-disable no-console */
 import { render } from '@testing-library/vue'
 import MaText from './MaText'
-import { text } from '../../tokens'
 import responsivePlugin from '@margarita/plugins/responsivePlugin'
 
-const CURRENT_BREAKPOINT = 'mobile' // this.$layout.currentBreakpoint is 'mobile' by default
-const { textSize: TEXT_SIZE_TOKENS } = text
 const slotText = 'Hello everybody'
 
 describe('MaText', () => {
@@ -15,28 +11,41 @@ describe('MaText', () => {
     expect(getByText(slotText)).toBeInTheDocument()
   })
 
-  test(`'tag' property test renders the valid provided tag element`, () => {
+  test(`'tag' property renders the valid provided tag element`, () => {
     const { getByText } = renderComponent({ tag: 'label' })
 
     expect(getByText(slotText).nodeName).toBe('LABEL')
   })
 
-  test(`'size' property test assigns the valid provided size styles to text`, () => {
+  test(`'size' property assigns the valid provided size styles to text`, () => {
     const { getByText } = renderComponent({
       size: 'medium',
     })
 
-    expect(getByText(slotText)).toHaveStyle(
-      TEXT_SIZE_TOKENS[CURRENT_BREAKPOINT]['medium']
-    )
+    expect(getByText(slotText)).toHaveStyle({
+      'font-size': '1.125rem',
+      'line-height': '1.35',
+    })
   })
 
-  test(`'tone' property test assigns the provided style to text`, () => {
+  test(`'tone' property assigns the provided style to text`, () => {
     const { getByText } = renderComponent({
       tone: 'gray',
     })
 
     expect(getByText(slotText)).toHaveStyle({ color: 'rgb(118, 118, 118)' }) //jest converts hex colors to rgb
+  })
+
+  test(`'bold' property renders ma-text--bold class`, () => {
+    const { getByText } = renderComponent({ bold: true })
+
+    expect(getByText(slotText)).toHaveClass('ma-text--bold')
+  })
+
+  test(`'italic' property renders ma-text--italic class`, () => {
+    const { getByText } = renderComponent({ italic: true })
+
+    expect(getByText(slotText)).toHaveClass('ma-text--italic')
   })
 })
 
