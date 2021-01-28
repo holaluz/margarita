431fdfd27f6b1acf38856e807eace6f136e15131
cillascillas <cillas.subira@holaluz.com>
2021-01-28 17:09:02 +0100

    Add introduction docs to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,13 @@
 // main.js is the main point of configuration for storybook
 
 module.exports = {
-  stories: ['../src/**/*.stories.js'],
+  stories: ['../src/**/*.stories.{js,mdx}'],
   addons: [
     '@storybook/addon-a11y',
     '@storybook/addon-actions',
     '@storybook/addon-knobs',
     '@storybook/addon-links',
     '@storybook/addon-viewport',
+    '@storybook/addon-docs',
   ],
 }
