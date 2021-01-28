0d6a204b79fc259c8ad801e8b65f5b1e01cf8b39
Eduard Farre Puig <edu.farrep@gmail.com>
2021-01-28 17:32:06 +0100

    Modify MaText story to display line-height cropping

--- src/components/MaText/MaText.stories.js
+++ src/components/MaText/MaText.stories.js
@@ -10,13 +10,17 @@ export default {
 export const Text = () => {
   const allowedTextSizes = Object.keys(text.textSize.mobile)
   const tag = select('Tag', textTags, 'span')
-  const tone = select('Tone', Object.keys(tones), 'neutral')
+  const tone = select('Tone', Object.keys(tones), 'gray')
   const italic = boolean('Italic', false)
   const bold = boolean('Bold', false)
 
   return {
     template: `
-        <ma-stack space="small">
+      <div style="display: flex;">
+        <ma-stack space="medium">
+          <ma-text size="medium">
+            MaText with different sizes
+          </ma-text>
           <ma-text
             v-for="textSize in allowedTextSizes"
             :key="textSize"
@@ -26,9 +30,27 @@ export const Text = () => {
             :bold="bold"
             :tone="tone"
           >
-            {{ textSize }} example text
+            {{ textSize }} size example text
           </ma-text>
         </ma-stack>
+        <ma-stack space="medium" style="margin-left: 4rem;">
+          <ma-text size="medium">
+            MaText displaying used line-height
+          </ma-text>
+          <ma-text
+            v-for="textSize in allowedTextSizes"
+            :key="textSize"
+            :size="textSize"
+            :tag="tag"
+            :italic="italic"
+            :bold="bold"
+            :tone="tone"
+            style="background-color:var(--color-blue-light)"
+          >
+            {{ textSize }} Example with UpperCase and LowerCase
+          </ma-text>
+        </ma-stack>
+      </div>
       `,
 
     data() {
