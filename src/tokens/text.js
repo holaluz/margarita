e3489a232750b0562c5e30a044a096fea888d829
Eduard Farre Puig <edu.farrep@gmail.com>
2021-01-28 17:27:11 +0100

    Add line-height crop values

--- src/tokens/text.js
+++ src/tokens/text.js
@@ -4,14 +4,20 @@ const smallScreenTextSize = {
   xsmall: {
     'font-size': '0.75rem',
     'line-height': 1.35,
+    'top-crop': '-0.35em',
+    'bottom-crop': '-0.35em',
   },
   small: {
     'font-size': '1rem',
     'line-height': 1.35,
+    'top-crop': '-0.3em',
+    'bottom-crop': '-0.3em',
   },
   medium: {
     'font-size': '1.125rem',
     'line-height': 1.35,
+    'top-crop': '-0.33em',
+    'bottom-crop': '-0.33em',
   },
 }
 
@@ -22,14 +28,20 @@ const textSize = {
     xsmall: {
       'font-size': '0.75rem',
       'line-height': 1.35,
+      'top-crop': '-0.3em',
+      'bottom-crop': '-0.33em',
     },
     small: {
       'font-size': '1rem',
       'line-height': 1.35,
+      'top-crop': '-0.3em',
+      'bottom-crop': '-0.3em',
     },
     medium: {
       'font-size': '1.25rem',
       'line-height': 1.35,
+      'top-crop': '-0.35em',
+      'bottom-crop': '-0.32em',
     },
   },
 }
